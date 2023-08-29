5448e7f0038a97ac07b90d820f7a9bc8b44a792a
jcoffee4477 <jacobcoffee@jacobs-air.mynetworksettings.com>
2023-08-29 14:52:03 -0400

    added scripts

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 const dayjs = require('dayjs')
 console.log(dayjs('2018-08-08'))
+dayjs().startOf('month').add(1, 'day').set('year', 2018).format('YYYY-MM-DD HH:mm:ss');
+
+
+console.log("hello world")
+
+
+
+function square() {
+  let example = () => {
+    let numbers = [];
+    for (let number of arguments) {
+      numbers.push(number * number);
+    }
+
+    return numbers;
+  };
+
+  return example();
+}
+
+console.log(square(2, 4, 7.5, 8, 11.5, 21))
+
+
+console.log("hellooooooooooooooooooooo")
