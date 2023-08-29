a0b7569430ca1b45a2a6d58b77d058b8fade78ab
jcoffee4477 <jacobcoffee@jacobs-air.mynetworksettings.com>
2023-08-29 13:22:49 -0400

    add webpack

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
 const dayjs = require('dayjs')
-
-console.log("hello world")
-
 console.log(dayjs('2018-08-08'))
-
-console.log(dayjs().startOf('month').add(1, 'day').set('year', 2018).format('YYYY-MM-DD HH:mm:ss'));
